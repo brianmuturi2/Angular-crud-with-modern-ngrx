ac59db7ea5bc608badd47844530dfe214c7dc3b3
brianmuturi2 <brianmuturi2@gmail.com>
2022-05-19 16:20:20 +0300

    feat(auth): created auth action & reducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,21 +1,29 @@
 import {
   ActionReducer,
   ActionReducerMap,
-  createFeatureSelector,
+  createFeatureSelector, createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer, on
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import {User} from '../model/user.model';
+import {AuthActions} from '../action-types';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
+export interface AuthState {
+  user: User;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState = {
+  user: undefined
 };
 
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const authReducer = createReducer(
+    initialAuthState,
+    on(AuthActions.login, (state, action) => {
+      return {
+        user: action.user
+      };
+    })
+);
